b77afce1398045958e9e8f97783355aad7c7afbd
Wunshan Seh <nlpguyz@gmail.com>
2018-02-15 11:29:21 -0800

    Fix for Andoir NDK r14+

--- src/operations.cpp
+++ src/operations.cpp
@@ -11,6 +11,8 @@
 //--------------------------------------------------------------------------------------// 
 
 //  define 64-bit offset macros BEFORE including boost/config.hpp (see ticket #5355) 
+
+#if !(defined(__ANDROID__) && __ANDROID_API__ < 21)
 #if !(defined(__HP_aCC) && defined(_ILP32) && !defined(_STATVFS_ACPP_PROBLEMS_FIXED))
 #define _FILE_OFFSET_BITS 64 // at worst, these defines may have no effect,
 #endif
@@ -27,6 +29,7 @@
       // systems as well.
 #else
 #define _FILE_OFFSET_BITS 64
+#endif
 #endif
 
 // define BOOST_FILESYSTEM_SOURCE so that <boost/filesystem/config.hpp> knows
